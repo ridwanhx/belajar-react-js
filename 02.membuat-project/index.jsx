d411f5f04f4aae8e6612887abc4b4ff39b9c3953
muhamadridwan1414 <mr1401.ridwan@gmail.com>
2025-12-02 00:58:10 +0700

    Bag. 02 - Belajar React JS : membuat project

---
+++ 02.membuat-project/index.jsx
@@ -0,0 +1,20 @@
+// # Membuat Project
+
+// Untuk membuat project React, kita akan menggunakan Vite sebagai build tool nya
+// Kita bisa membuat project React menggunakan beberapa perintah
+
+// Untuk membuat project React menggunakan JavaScript :
+// npm create vite@latest nama-aplikasi -- --template react
+
+// Untuk membuat project React menggunakan TypeScript :
+// npm create vite@latest nama-aplikasi -- --template react-ts
+
+// Tunggu sampai selesai, kemudian jalankan :
+// npm install
+// Lalu jika ingin menjalankan project nya bisa dengan menggunakan perintah :
+// npm run dev
+
+// pada file package.json
+// pastikan versi react nya sudah yang terbaru, jika masih menggunakan versi lama atau dibawah versi 19, maka update terlebih dahulu
+// https://www.npmjs.com/package/react
+// https://www.npmjs.com/package/react-dom

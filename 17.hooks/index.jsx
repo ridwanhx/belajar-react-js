85a11fd26c4a592d48f9be6e3383bfbcba291dcb
muhamadridwan1414 <mr1401.ridwan@gmail.com>
2025-12-17 23:51:46 +0700

    Bag. 17 - Belajar React JS : react hooks (intro)

---
+++ 17.hooks/index.jsx
@@ -0,0 +1,5 @@
+// # Hooks
+
+// Hooks adalah fitur di React yang bisa digunakan di dalam Component
+// Ada banyak sekali fitur yang bsia kita gunakan di React Hooks, dan kita akan bahas secara bertahap
+// https://react.dev/reference/react/hooks
